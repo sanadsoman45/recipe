6d19af3044108e149455b9e11b89a6417482a6d5
SS <43528889+sanadsoman45@users.noreply.github.com>
2021-02-05 17:00:40 +0530

    more buttons added

--- app/src/main/java/Adapter/MyAdapter.java
+++ app/src/main/java/Adapter/MyAdapter.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,7 +25,7 @@ import Model.ListItem;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private final Context con;
     private final List<ListItem> listitems;
-
+    private int i=10;
     public MyAdapter(Context con, List<ListItem> listitems) {
         this.con = con;
         this.listitems = listitems;
@@ -44,93 +45,67 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         ListItem lst=listitems.get(position);
         holder.titletv.setText(lst.getTitle());
         FlexboxLayout layout = holder.flb;
-        int i=10;
+
         if(lst.getbtn_size()>10) {
-
-            for(int j=0;j<=i;j++) {
-
-                Log.d("clickmsg","value of i is:"+i);
-                ToggleButton btnTag = new ToggleButton(con);
-                btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                btnTag.setText(lst.getBtn(j));
-                btnTag.setTextOn(lst.getBtn(j));
-                btnTag.setTextOff(lst.getBtn(j));
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                lp.setMargins(10, 10, 10, 10);
-                btnTag.setLayoutParams(lp);
-                int id=Integer.parseInt((String.valueOf(j)+ position));
-                btnTag.setId(id);
-                btnTag.setBackgroundDrawable(con.getDrawable(R.drawable.ic_toggle));
-                btnTag.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if(buttonView.isChecked()){
-                            Toast.makeText(con, btnTag.getText(), Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(con,"Else Block", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-                int check=0;
-                if(check==0){
-                    Log.e("checkval1", ""+check);
-                    if(j==10){
-                        String remainingquantity=String.valueOf((lst.getbtn_size()-10));
-                        btnTag.setText(String.format("%s more", remainingquantity));
-                        btnTag.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View v) {
-                                Log.e("checkval2", ""+check);
-                                i= lst.getbtn_size();
-                                Log.d("onclickmsg","value of i is:"+i);
-                                btnTag.setChecked(false);
-                                check=1;
+            for(int j=0;j<=10;j++){
+                if(j==10){
+                    Button btnTag=new Button(con);
+                    btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    btnTag.setText("+"+(lst.getbtn_size()-10)+"more");
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    lp.setMargins(10, 10, 10, 10);
+                    btnTag.setLayoutParams(lp);
+                    btnTag.setBackground(con.getDrawable(R.drawable.ic_normalbutton));
+                    layout.addView(btnTag);
+                    btnTag.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            for(int j=10;j<lst.getbtn_size();j++){
+                                Log.d("msginside","Value of j is:"+j);
+                                create_Togglebtn(layout,lst,j);
+                                layout.removeView(btnTag);
                             }
-                        });
-                    }
+                        }
+                    });
+                }
+                else{
+                    create_Togglebtn(layout,lst,j);
                 }
 
-//                Log.d("msg1","id Of Button at position"+position+"is: "+btnTag.getId());
-//                Log.d("msgid", String.valueOf(btnTag.getId()));
-                Log.d("msgadapter","Value of j is:"+j);
 
-                layout.addView(btnTag);
             }
         }
         else{
             for (int j = 0; j < lst.getbtn_size(); j++) {
-                ToggleButton btnTag = new ToggleButton(con);
-                btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                btnTag.setText(lst.getBtn(j));
-                btnTag.setTextOn(lst.getBtn(j));
-                btnTag.setTextOff(lst.getBtn(j));
-                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                lp.setMargins(10, 10, 10, 10);
-                btnTag.setLayoutParams(lp);
-                int id=Integer.parseInt((String.valueOf(j)+ position));
-                btnTag.setId(id);
-                btnTag.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                    @Override
-                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                        if(buttonView.isChecked()){
-                            Toast.makeText(con, btnTag.getText(), Toast.LENGTH_SHORT).show();
-                        }
-                        else{
-                            Toast.makeText(con,"Else Block", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-                Log.d("msg1","id Of Button at position"+position+"is: "+btnTag.getId());
-                Log.d("msgid", String.valueOf(btnTag.getId()));
-                Log.d("msgadapter","msgadapter"+lst.getBtn(j)+"Value of j is:"+j);
-                btnTag.setBackgroundDrawable(con.getDrawable(R.drawable.ic_toggle));
-                layout.addView(btnTag);
+                create_Togglebtn(layout,lst,j);
             }
         }
 
 
+    }
+
+    public void create_Togglebtn(FlexboxLayout layout,ListItem lst,int j){
+        ToggleButton btnTag = new ToggleButton(con);
+        btnTag.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        btnTag.setText(lst.getBtn(j));
+        btnTag.setTextOn(lst.getBtn(j));
+        btnTag.setTextOff(lst.getBtn(j));
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(10, 10, 10, 10);
+        btnTag.setLayoutParams(lp);
+        btnTag.setBackgroundDrawable(con.getDrawable(R.drawable.ic_toggle));
+        btnTag.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(buttonView.isChecked()){
+                    Toast.makeText(con, btnTag.getText(), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(con,"Else Block", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        layout.addView(btnTag);
     }
 
     @Override
